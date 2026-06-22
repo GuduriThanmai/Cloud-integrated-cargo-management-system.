80a3d9b27d961106373ddb5779704fbdee63431b
GuduriThanmai <gudurithanmai1@gmail.com>
2026-06-23 00:07:38 +0530

    sample_data.sql

--- database/sample_data.sql
+++ database/sample_data.sql
@@ -1,1 +1,29 @@
+CREATE TABLE users(
+id INTEGER PRIMARY KEY,
+username TEXT,
+email TEXT,
+password TEXT
+);
 
+CREATE TABLE cargo(
+cargo_id INTEGER PRIMARY KEY,
+cargo_name TEXT,
+weight REAL,
+status TEXT
+);
+
+CREATE TABLE shipment(
+shipment_id INTEGER PRIMARY KEY,
+tracking_number TEXT,
+source TEXT,
+destination TEXT
+);
+
+CREATE TABLE sensordata(
+sensor_id INTEGER PRIMARY KEY,
+temperature REAL,
+humidity REAL,
+pressure REAL,
+gas_value INTEGER,
+timestamp DATETIME
+);
